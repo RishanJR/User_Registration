6c12c74c4bac7c53e928ac8374e9089422301c3e
RishanJR <rishanrajkumar@yahoo.com>
2020-09-27 11:25:35 +0400

    Checking the first name of the user

--- User_registration.sh
+++ User_registration.sh
@@ -2,3 +2,17 @@
 
 echo -e "\nWelcome to User Registration"
 echo -e "This program ensures all the validations are in place during the User Entry\n"
+
+echo "Enter your first name (staring with caps and having a minimum of 3 characters)"
+read first_name
+
+#Pattern to check if first letter is uppercase and has a min of 3 characters
+pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+#Checking if name matches the pattern
+if [[ $first_name =~ $pat ]]
+then
+	echo -e "\nThe first name $first_name is valid"
+else
+	echo -e "\nThe first name $first_name is invalid"
+fi
