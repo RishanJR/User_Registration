b69bd26d05380cb906712b12c8493ee11a92e7d4
RishanJR <rishanrajkumar@yahoo.com>
2020-09-27 11:31:37 +0400

    Checking if users last name matches the pattern (first letter caps and min 3 characters)

--- User_registration.sh
+++ User_registration.sh
@@ -3,16 +3,28 @@
 echo -e "\nWelcome to User Registration"
 echo -e "This program ensures all the validations are in place during the User Entry\n"
 
-echo "Enter your first name (staring with caps and having a minimum of 3 characters)"
+#Function to check if the name starts with caps and has min 3 characters
+function check_name() {
+
+	#Pattern to check if first letter is uppercase and has a min of 3 characters
+	pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+	#Checking if name matches the pattern
+	if [[ $1 =~ $pat ]]
+	then
+		echo -e "\nThe name is valid"
+	else
+		echo -e "\nThe name is invalid"
+	fi
+
+}
+
+echo "Enter your first name"
 read first_name
 
-#Pattern to check if first letter is uppercase and has a min of 3 characters
-pat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+check_name $first_name
 
-#Checking if name matches the pattern
-if [[ $first_name =~ $pat ]]
-then
-	echo -e "\nThe first name $first_name is valid"
-else
-	echo -e "\nThe first name $first_name is invalid"
-fi
+echo -e "\nEnter your last name"
+read last_name
+
+check_name $last_name
